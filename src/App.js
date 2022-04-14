4a14304a2f2bd5c42ca70db5e9f0597f85391328
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-04-14 21:49:33 +0530

    fixeding netlify

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/header/NavBar.js";
 import Sidebar from "./components/sidebar/sidebar.js";
 
-import HomePage from "./";
+import HomePage from "./components/HomePage/index.jsx";
 import HistoryPage from "./components/HistoryPage/index";
-import LikedPage from "./pages/LikedPage/index";
-import PlaylistPage from "./pages/PlaylistPage/index";
-import ExplorePage from "./pages/ExplorePage/index";
+import LikedPage from "./components/LikedPage/index";
+import PlaylistPage from "./components/PlaylistPage/index";
+import ExplorePage from "./components/ExplorePage/index";
 
 import Login from "./components/AuthenticationPages/login";
 import Signup from "./components/AuthenticationPages/signup";
