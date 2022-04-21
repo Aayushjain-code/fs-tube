d30b22d0d4816e21fcc127065a1fa0ed5615355c
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-04-21 16:45:00 +0530

    Loader Updated

--- src/assets/loader/Loader.js
+++ src/assets/loader/Loader.js
@@ -1,297 +1,40 @@
-import * as React from "react"
+import * as React from "react";
 
-const LoaderSvg = () => (
+const LoaderSvg = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     style={{
       margin: "auto",
-      background: "#fff",
+      background: "0 0",
       display: "block",
       shapeRendering: "auto",
-      animationPlayState: "running",
-      animationDelay: "0s",
     }}
-    width={200}
-    height={200}
+    width={97}
+    height={97}
     viewBox="0 0 100 100"
     preserveAspectRatio="xMidYMid"
+    {...props}
   >
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
+    <circle
+      cx={50}
+      cy={50}
+      r={42}
+      strokeWidth={4}
+      stroke="#07ba51"
+      strokeDasharray="65.97344572538566 65.97344572538566"
+      fill="none"
+      strokeLinecap="round"
     >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.9166666666666666s"
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
         repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(30 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
         dur="1s"
-        begin="-0.8333333333333334s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(60 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
         keyTimes="0;1"
-        dur="1s"
-        begin="-0.75s"
-        repeatCount="indefinite"
+        values="0 50 50;360 50 50"
       />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(90 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.6666666666666666s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(120 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.5833333333333334s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(150 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.5s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(180 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.4166666666666667s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(210 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.3333333333333333s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(240 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.25s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(270 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.16666666666666666s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(300 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="-0.08333333333333333s"
-        repeatCount="indefinite"
-      />
-    </rect>
-    <rect
-      x={47}
-      y={24}
-      rx={3}
-      ry={6}
-      width={6}
-      height={12}
-      fill="#07ba51"
-      style={{
-        animationPlayState: "running",
-        animationDelay: "0s",
-      }}
-      transform="rotate(330 50 50)"
-    >
-      <animate
-        attributeName="opacity"
-        values="1;0"
-        keyTimes="0;1"
-        dur="1s"
-        begin="0s"
-        repeatCount="indefinite"
-      />
-    </rect>
+    </circle>
   </svg>
-)
+);
 
-export default LoaderSvg
+export default LoaderSvg;
