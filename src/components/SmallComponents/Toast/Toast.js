5a20d1e8fe672860510563f4b8272130116b6b91
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-05-05 10:12:31 +0530

    basics of toast and playlist

---
+++ src/components/SmallComponents/Toast/Toast.js
@@ -0,0 +1,19 @@
+import "./Toast.css";
+import { useEffect } from "react";
+
+export const Toast = ({ toast, handleToastClose }) => {
+  const { _id, status, msg } = toast;
+
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      handleToastClose();
+    }, 3000);
+    return () => {
+      clearTimeout(timeoutID);
+    };
+  }, []);
+
+  return (
+    <div className={`text-toast ${status} flex flex-justify-center`}>{msg}</div>
+  );
+};
